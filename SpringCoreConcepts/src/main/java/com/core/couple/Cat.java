b219d4f05dd9c184ee721500575d99357f3f8037
vikashbasant <basantvikash360@gmail.com>
2024-10-03 14:45:13 +0530

    _fun: concept to coupling

--- SpringCoreConcepts/src/main/java/com/core/couple/Cat.java
+++ SpringCoreConcepts/src/main/java/com/core/couple/Cat.java
@@ -1,9 +1,12 @@
 package com.core.couple;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Component("cat")
+@Component
+//@Primary
+@Qualifier("cat")
 public class Cat implements Animal{
     @Override
     public void play() {
